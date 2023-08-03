ebc1fb0573995a7f82a27f7b6fe5aff7be3634b8
KuoAnn <b54744040@gmail.com>
2023-08-03 14:27:15 +0800

    Update Gengo-Trans.user.js

--- src/Gengo-Trans.user.js
+++ src/Gengo-Trans.user.js
@@ -2,7 +2,7 @@
 // @name         Gengo-Trans
 // @source       https://github.com/KuoAnn/TampermonkeyUserscripts/raw/main/src/Gengo-Trans.user.js
 // @namespace    https://gengo.com/
-// @version      1.4.7
+// @version      1.4.8
 // @description  Gengo Translate Extensions
 // @author       KuoAnn
 // @match        https://gengo.com/t/workbench/*
@@ -20,17 +20,17 @@ const css = `
     .btn-all {float:right;}
 `;
 const svg_clone =
-    '<svg width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M9 18q-.825 0-1.413-.588T7 16V4q0-.825.588-1.413T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.588 1.413T18 18H9Zm0-2h9V4H9v12Zm-6-1h2v-2H3v2Zm0-3.5h2v-2H3v2ZM10 22h2v-2h-2v2Zm-7-3.5h2v-2H3v2ZM5 22v-2H3q0 .825.588 1.413T5 22Zm1.5 0h2v-2h-2v2Zm7 0q.825 0 1.413-.588T15.5 20h-2v2ZM3 8h2V6q-.825 0-1.413.588T3 8Z"/></svg>';
+      '<svg width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M9 18q-.825 0-1.413-.588T7 16V4q0-.825.588-1.413T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.588 1.413T18 18H9Zm0-2h9V4H9v12Zm-6-1h2v-2H3v2Zm0-3.5h2v-2H3v2ZM10 22h2v-2h-2v2Zm-7-3.5h2v-2H3v2ZM5 22v-2H3q0 .825.588 1.413T5 22Zm1.5 0h2v-2h-2v2Zm7 0q.825 0 1.413-.588T15.5 20h-2v2ZM3 8h2V6q-.825 0-1.413.588T3 8Z"/></svg>';
 const svg_highlight =
-    '<svg width="18" height="18" viewBox="0 0 32 32"><path fill="currentColor" d="M12 15H5a3 3 0 0 1-3-3v-2a3 3 0 0 1 3-3h5V5a1 1 0 0 0-1-1H3V2h6a3 3 0 0 1 3 3zM5 9a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5V9zm15 14v2a1 1 0 0 0 1 1h5v-4h-5a1 1 0 0 0-1 1z"/><path fill="currentColor" d="M2 30h28V2Zm26-2h-7a3 3 0 0 1-3-3v-2a3 3 0 0 1 3-3h5v-2a1 1 0 0 0-1-1h-6v-2h6a3 3 0 0 1 3 3Z"/></svg>';
+      '<svg width="18" height="18" viewBox="0 0 32 32"><path fill="currentColor" d="M12 15H5a3 3 0 0 1-3-3v-2a3 3 0 0 1 3-3h5V5a1 1 0 0 0-1-1H3V2h6a3 3 0 0 1 3 3zM5 9a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5V9zm15 14v2a1 1 0 0 0 1 1h5v-4h-5a1 1 0 0 0-1 1z"/><path fill="currentColor" d="M2 30h28V2Zm26-2h-7a3 3 0 0 1-3-3v-2a3 3 0 0 1 3-3h5v-2a1 1 0 0 0-1-1h-6v-2h6a3 3 0 0 1 3 3Z"/></svg>';
 const svg_trans =
-    '<svg width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M5 15v2a2 2 0 0 0 1.85 1.994L7 19h3v2H7a4 4 0 0 1-4-4v-2h2Zm13-5l4.4 11h-2.155l-1.201-3h-4.09l-1.199 3h-2.154L16 10h2Zm-1 2.885L15.753 16h2.492L17 12.885ZM8 2v2h4v7H8v3H6v-3H2V4h4V2h2Zm9 1a4 4 0 0 1 4 4v2h-2V7a2 2 0 0 0-2-2h-3V3h3ZM6 6H4v3h2V6Zm4 0H8v3h2V6Z"/></svg>';
+      '<svg width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M5 15v2a2 2 0 0 0 1.85 1.994L7 19h3v2H7a4 4 0 0 1-4-4v-2h2Zm13-5l4.4 11h-2.155l-1.201-3h-4.09l-1.199 3h-2.154L16 10h2Zm-1 2.885L15.753 16h2.492L17 12.885ZM8 2v2h4v7H8v3H6v-3H2V4h4V2h2Zm9 1a4 4 0 0 1 4 4v2h-2V7a2 2 0 0 0-2-2h-3V3h3ZM6 6H4v3h2V6Zm4 0H8v3h2V6Z"/></svg>';
 const svg_cn2tw =
-    '<svg width="18" height="18" viewBox="0 0 64 64"><path fill="currentColor" d="M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30s30-13.432 30-30S48.568 2 32 2M19.217 24.134a4.916 4.916 0 1 1 0-9.834c2.716 0 4.916 2.201 4.916 4.917s-2.2 4.917-4.916 4.917m1.169.529l-1.169 4.387l-1.171-4.388c.378.081.769.126 1.171.126c.401 0 .792-.045 1.169-.125m-5.297-9.183a5.565 5.565 0 0 0-1.175 2.024L10.701 14.3l4.388 1.18m-.789-4.779l3.205 3.213a5.542 5.542 0 0 0-2.024 1.175L14.3 10.701m-.531 7.345a5.56 5.56 0 0 0 0 2.34l-4.386-1.169l4.386-1.171m.145 2.882c.245.76.651 1.445 1.175 2.023l-4.388 1.183l3.213-3.206m1.566 2.415a5.58 5.58 0 0 0 2.025 1.176L14.3 27.732l1.18-4.389m2.566-9.574l1.171-4.385l1.169 4.385a5.56 5.56 0 0 0-2.34 0m4.905 1.32a5.569 5.569 0 0 0-2.023-1.175l3.205-3.213l-1.182 4.388m4.781-.789l-3.214 3.206a5.557 5.557 0 0 0-1.176-2.025l4.39-1.181m-4.781 9.043l1.182 4.39l-3.207-3.214a5.585 5.585 0 0 0 2.025-1.176m.391-.392a5.567 5.567 0 0 0 1.177-2.025l3.214 3.208l-4.391-1.183m1.321-2.565c.08-.377.125-.768.125-1.169c0-.402-.045-.793-.126-1.171l4.388 1.171l-4.387 1.169M32 60C16.561 60 4 47.439 4 32h28V4c15.439 0 28 12.561 28 28S47.439 60 32 60"/></svg>';
+      '<svg width="18" height="18" viewBox="0 0 64 64"><path fill="currentColor" d="M32 2C15.432 2 2 15.432 2 32s13.432 30 30 30s30-13.432 30-30S48.568 2 32 2M19.217 24.134a4.916 4.916 0 1 1 0-9.834c2.716 0 4.916 2.201 4.916 4.917s-2.2 4.917-4.916 4.917m1.169.529l-1.169 4.387l-1.171-4.388c.378.081.769.126 1.171.126c.401 0 .792-.045 1.169-.125m-5.297-9.183a5.565 5.565 0 0 0-1.175 2.024L10.701 14.3l4.388 1.18m-.789-4.779l3.205 3.213a5.542 5.542 0 0 0-2.024 1.175L14.3 10.701m-.531 7.345a5.56 5.56 0 0 0 0 2.34l-4.386-1.169l4.386-1.171m.145 2.882c.245.76.651 1.445 1.175 2.023l-4.388 1.183l3.213-3.206m1.566 2.415a5.58 5.58 0 0 0 2.025 1.176L14.3 27.732l1.18-4.389m2.566-9.574l1.171-4.385l1.169 4.385a5.56 5.56 0 0 0-2.34 0m4.905 1.32a5.569 5.569 0 0 0-2.023-1.175l3.205-3.213l-1.182 4.388m4.781-.789l-3.214 3.206a5.557 5.557 0 0 0-1.176-2.025l4.39-1.181m-4.781 9.043l1.182 4.39l-3.207-3.214a5.585 5.585 0 0 0 2.025-1.176m.391-.392a5.567 5.567 0 0 0 1.177-2.025l3.214 3.208l-4.391-1.183m1.321-2.565c.08-.377.125-.768.125-1.169c0-.402-.045-.793-.126-1.171l4.388 1.171l-4.387 1.169M32 60C16.561 60 4 47.439 4 32h28V4c15.439 0 28 12.561 28 28S47.439 60 32 60"/></svg>';
 const svg_pangu =
-    '<svg width="18" height="18" viewBox="0 0 256 256"><path fill="currentColor" d="M112 48v160a8 8 0 0 1-16 0v-72H43.31l18.35 18.34a8 8 0 0 1-11.32 11.32l-32-32a8 8 0 0 1 0-11.32l32-32a8 8 0 0 1 11.32 11.32L43.31 120H96V48a8 8 0 0 1 16 0Zm125.66 74.34l-32-32a8 8 0 0 0-11.32 11.32L212.69 120H160V48a8 8 0 0 0-16 0v160a8 8 0 0 0 16 0v-72h52.69l-18.35 18.34a8 8 0 0 0 11.32 11.32l32-32a8 8 0 0 0 0-11.32Z"/></svg>';
+      '<svg width="18" height="18" viewBox="0 0 256 256"><path fill="currentColor" d="M112 48v160a8 8 0 0 1-16 0v-72H43.31l18.35 18.34a8 8 0 0 1-11.32 11.32l-32-32a8 8 0 0 1 0-11.32l32-32a8 8 0 0 1 11.32 11.32L43.31 120H96V48a8 8 0 0 1 16 0Zm125.66 74.34l-32-32a8 8 0 0 0-11.32 11.32L212.69 120H160V48a8 8 0 0 0-16 0v160a8 8 0 0 0 16 0v-72h52.69l-18.35 18.34a8 8 0 0 0 11.32 11.32l32-32a8 8 0 0 0 0-11.32Z"/></svg>';
 const img_promtGpt =
-    '<img style="display:block;height:18px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/240px-ChatGPT_logo.svg.png">';
+      '<img style="display:block;height:18px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/240px-ChatGPT_logo.svg.png">';
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
@@ -170,16 +170,16 @@ let OPENAI_API_KEY = localStorage.getItem("OPENAI_API_KEY");
                 console.log("Append Source[" + i + "]");
                 $d.find(".source-wrap pre").before(
                     "<div class='sourceToolbar'>" +
-                        '<a class="btn btnSrcCopy btn-link" href="javascript:void(0)">' +
-                        svg_clone +
-                        "</a>" +
-                        '<a class="btn btnPromtGpt btn-link" href="javascript:void(0)">' +
-                        img_promtGpt +
-                        "</a>" +
-                        '<a class="btn btnHighlight btn-link" href="javascript:void(0)">' +
-                        svg_highlight +
-                        "</a>" +
-                        "</div>"
+                    '<a class="btn btnSrcCopy btn-link" href="javascript:void(0)">' +
+                    svg_clone +
+                    "</a>" +
+                    '<a class="btn btnPromtGpt btn-link" href="javascript:void(0)">' +
+                    img_promtGpt +
+                    "</a>" +
+                    '<a class="btn btnHighlight btn-link" href="javascript:void(0)">' +
+                    svg_highlight +
+                    "</a>" +
+                    "</div>"
                 );
                 // Target
                 console.log("Append Target[" + i + "]");
@@ -193,9 +193,9 @@ let OPENAI_API_KEY = localStorage.getItem("OPENAI_API_KEY");
             // Source
             $(".btnHighlight")
                 .click(function () {
-                    let $target = $(this).parents(".source-wrap").find("pre br");
-                    $target.before("<mark>");
-                })
+                let $target = $(this).parents(".source-wrap").find("pre br");
+                $target.before("<mark>");
+            })
                 .click();
             $(".btnPromtGpt").click(function () {
                 let $target = $(this).parents(".source-wrap").find("pre");
@@ -289,10 +289,10 @@ function panguText($area) {
 function promt2Gpt(text) {
     if (text) {
         let prompt = text
-            .replace(/\r/g, "")
-            .replace(/\s+$/g, "")
-            .replace(/\n{3,}/gs, "\n\n")
-            .replace(/^\s+|\s+$/gs, "");
+        .replace(/\r/g, "")
+        .replace(/\s+$/g, "")
+        .replace(/\n{3,}/gs, "\n\n")
+        .replace(/^\s+|\s+$/gs, "");
 
         prompt = `將以下內容翻譯成臺灣常用的正體中文：\n\n${prompt}`;
         var url = `https://chat.openai.com/chat#autoSubmit=1&prompt=${encodeURIComponent(prompt)}`;
@@ -333,15 +333,18 @@ function send2Gpt(sQuestion, $destination) {
                 oJson = JSON.parse(oHttp.responseText);
             } catch (ex) {
                 alert("GPT Parse Error: " + ex.message);
-                log.console("Resp:" + oHttp.responseText);
+                console.log("Resp:" + oHttp.responseText);
             }
 
             if (oJson.error && oJson.error.message) {
-                alert("GPT Error: " + oJson.error.message);
-                console.eror("GPT Error: " + oJson.error.message);
                 if (oJson.error.code == "invalid_api_key") {
+                    alert("ApiKey Invalid, plz Re-enter!")
                     OPENAI_API_KEY = null;
                     localStorage.removeItem("OPENAI_API_KEY");
+                }
+                else{
+                    alert("GPT Error: " + oJson.error.message);
+                    console.error("GPT Error: " + oJson.error.message);
                 }
             } else if (oJson.choices && oJson.choices[0].message) {
                 console.log(oJson);
