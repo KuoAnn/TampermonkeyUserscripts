9447f467b3d255ce66187c98c4df598cae5d360c
KuoAnn <b54744040@gmail.com>
2024-04-26 11:41:25 +0800

    [國泰學習網] 加入固定到期時間避免瀏覽器背景讀秒失效

--- src/Cathay-Learn.user.js
+++ src/Cathay-Learn.user.js
@@ -2,62 +2,75 @@
 // @name         國泰學習網
 // @namespace    http://tampermonkey.net/
 // @source       https://github.com/KuoAnn/TampermonkeyUserscripts/raw/main/src/Cathay-Learn.user.js
-// @version      1.0.1
+// @version      1.0.2
 // @description  try to take over the world!
 // @author       KuoAnn
 // @match        https://cathay.elearn.com.tw/cltcms/play-index-home.do
 // @grant        none
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.cathaybk.com.tw
 // ==/UserScript==
-
 (function () {
-  "use strict";
+    "use strict";
+    var body = document.querySelector("body");
+    var title = document.createElement("h2");
+    title.style.textAlign = "center";
+    title.style.position = "fixed";
+    title.style.width = "100%";
+    title.style.top = "24px";
+    title.style.color = "#eee";
+    title.style.backgroundColor = "red";
+    body.prepend(title);
 
-  var body = document.querySelector("body");
-  var h1 = document.createElement("H1");
-  h1.style.textAlign = "center";
-  h1.style.position = "fixed";
-  h1.style.width = "100%";
-  h1.style.top = "36px";
-  h1.style.color = "#eee";
-  h1.style.backgroundColor = "red";
-  body.prepend(h1);
-
-  var countdown = 0; //Sec
-
-  var waitTime = 30; //Sec
-  var onloadInt = setInterval(function () {
-    try {
-      let duration = document
-        .querySelector("iframe#content")
-        .contentWindow.document.querySelector("iframe#playContent")
-        .contentWindow.document.querySelector("iframe#Content")
-        .contentWindow.document.querySelector("video").duration;
-
-      countdown = Math.round(duration);
-    } catch (error) {}
-
-    if (countdown > 0) {
-      AutoLearn();
-    } else if (waitTime <= 0) {
-      countdown = 5 * 60; //default: 5 min
-      AutoLearn();
-    } else {
-      waitTime--;
-    }
-  }, 1000);
-
-  function AutoLearn() {
-    clearInterval(onloadInt);
-    setInterval(function () {
-      if (countdown > 0) {
-        h1.innerText = "Auto Learning..." + --countdown;
-      } else {
-        var x = document
-          .querySelector("iframe#banner")
-          .contentWindow.document.querySelectorAll("button");
-        x[x.length - 1].click();
-      }
+    var countdownSec = 0; //Sec
+    var waitTime = 15; //Sec
+    var onloadInt = setInterval(function () {
+        try {
+            let duration = document
+                .querySelector("iframe#content")
+                .contentWindow.document.querySelector("iframe#playContent")
+                .contentWindow.document.querySelector("iframe#Content")
+                .contentWindow.document.querySelector("video").duration;
+            countdownSec = Math.round(duration);
+        } catch (error) {}
+        if (countdownSec > 0) {
+            AutoLearn();
+        } else if (waitTime <= 0) {
+            //default: 5 min
+            countdownSec = 5 * 60;
+            AutoLearn();
+        } else {
+            waitTime--;
+        }
     }, 1000);
-  }
+
+    function AutoLearn() {
+        clearInterval(onloadInt);
+        // 調整視窗為 400*400
+        window.resizeTo(400, 400);
+        // 獲取螢幕寬高
+        const screenWidth = screen.width;
+        const screenHeight = screen.height;
+        const windowWidth = window.outerWidth;
+        const windowHeight = window.outerHeight;
+        // 計算右下角座標
+        const rightX = screenWidth - windowWidth;
+        const bottomY = screenHeight - windowHeight;
+        // 移動視窗到右下角
+        window.moveTo(rightX, bottomY);
+
+        var endDate = new Date();
+        endDate.setSeconds(endDate.getSeconds() + countdownSec);
+        console.log("Countdown to: " + endDate);
+
+        setInterval(function () {
+            // re-count countdownSec from endDate
+            countdownSec = Math.round((endDate - new Date()) / 1000);
+            if (countdownSec > 0) {
+                title.innerText = "Auto Learning..." + --countdownSec;
+            } else {
+                var x = document.querySelector("iframe#banner").contentWindow.document.querySelectorAll("button");
+                x[x.length - 1].click();
+            }
+        }, 1000);
+    }
 })();
