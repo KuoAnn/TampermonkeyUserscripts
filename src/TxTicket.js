3d979ba57744e005a8d449a6c668b7d6c157453b
KuoAnn <b54744040@gmail.com>
2024-09-25 17:54:47 +0800

    feat. 拓元售票輔助2

--- src/TxTicket.js
+++ src/TxTicket.js
@@ -4,40 +4,68 @@
 // @version      2024-09-25
 // @description  try to take over the world!
 // @author       You
-// @match        https://tixcraft.com/ticket/*
+// @match        https://tixcraft.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tixcraft.com
 // @grant        none
 // ==/UserScript==
 
 (function () {
     "use strict";
+    let checkCode = "40637634";
+    let isClickBuyTicket = false;
 
     const observer = new MutationObserver((mutationsList) => {
         mutationsList.forEach((mutation) => {
             if (mutation.type === "childList") {
+                // observer.disconnect(); // 偵測到後停止監聽
+
+                // 勾選同意條款
                 const ticketAgree = document.getElementById("TicketForm_agree");
                 if (ticketAgree) {
-                    // observer.disconnect(); // 偵測到後停止監聽
                     ticketAgree.checked = true;
-                    const ticketPrice = document.getElementById("TicketForm_ticketPrice_02");
-                    if (ticketPrice) {
-                        ticketPrice.value = 4;
-                    }
-
-                    document.getElementById("TicketForm_verifyCode").focus();
+                }
+                const verifyCodeInput = document.getElementById("TicketForm_verifyCode");
+                if (verifyCodeInput) {
+                    verifyCodeInput.focus();
+                    // 於輸入框按下 enter 點選 submit
+                    verifyCodeInput.addEventListener("keydown", function (e) {
+                        if (e.key === "Enter") {
+                            const submit = document.querySelector("button[type=submit],#submitButton");
+                            if (submit) {
+                                submit.click();
+                            }
+                        }
+                    });
                 }
 
-                /*
-                 * <a href="/activity/game/24_asmrmaxxx_c" target="_new"><div>立即購票</div></a>
-                 */
+                // 購票 4 張
+                const ticketPrice = document.querySelector("#TicketForm_ticketPrice_01,#TicketForm_ticketPrice_02");
+                if (ticketPrice) {
+                    ticketPrice.value = 4;
+                }
+
+                // 點選關閉提醒
+                const closeAlert = document.querySelector("button.close-alert");
+                if (closeAlert) {
+                    closeAlert.click();
+                }
+
+                // 點選立即購票
                 const buyTicket = document.querySelector('a[href^="/activity/game/"]');
-                if (buyTicket) {
+                if (buyTicket && !isClickBuyTicket) {
+                    isClickBuyTicket = true;
                     buyTicket.click();
                 }
 
+                // 輸入驗證碼：信用卡卡號
+                const checkCodeInput = document.querySelector(".greyInput[name=checkCode]");
+                if (checkCodeInput) {
+                    checkCodeInput.value = checkCode;
+                }
+
+                // submit button 變大
                 const submit = document.querySelector("button[type=submit],#submitButton");
                 if (submit) {
-                    // 將 button 變大
                     submit.style.fontSize = "20px";
                     submit.style.height = "100px";
                     submit.style.width = "200px";
@@ -47,4 +75,22 @@
     });
 
     observer.observe(document.body, { childList: true, subtree: true });
+
+    // alt+方向鍵下：選擇下一個售票日期
+    document.addEventListener("keydown", function (e) {
+        if (e.altKey && e.key === "ArrowDown") {
+            let select = document.querySelector("#gameId");
+            if (select) {
+                let selectedIndex = select.selectedIndex;
+                let optionsLength = select.options.length;
+
+                // Move to next option, or wrap around to the first one
+                select.selectedIndex = (selectedIndex + 1) % optionsLength;
+
+                // Trigger the change event
+                let event = new Event("change", { bubbles: true });
+                select.dispatchEvent(event);
+            }
+        }
+    });
 })();
