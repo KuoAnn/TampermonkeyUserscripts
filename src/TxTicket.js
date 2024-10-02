c7fab4ca1b29a26fc9ef35ee7a10a5e148debc55
KuoAnn <b54744040@gmail.com>
2024-10-03 00:02:06 +0800

    feat. TxTicket

--- src/TxTicket.js
+++ src/TxTicket.js
@@ -1,146 +1,306 @@
 // ==UserScript==
-// @name         拓元售票輔助
+// @name         TxTicket
 // @namespace    http://tampermonkey.net/
-// @version      2024-09-25
+// @version      1.0.0
 // @description  1. 自動勾選同意條款 2. 自動輸入驗證碼 3. 自動選取購票 n 張 4. 自動點選立即購票 5. 自動選擇付款方式 6. 提交按鈕變大 7. alt+方向鍵下：選擇下一個售票日期 8. Enter 鍵直接送出 9. 自動關閉提醒
 // @author       You
 // @match        https://tixcraft.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tixcraft.com
 // @grant        none
 // ==/UserScript==
+// 個人參數
+const buyDateIndexes = [2, 3]; // 場次優先順序：0=第一場 1=第二場...；若無則預設最後一場
+const buyArea = ["VIP"]; // 座位優先順序，建議嚴謹>鬆散；以空白作為 AND 邏輯：空值=不限
+const buyCount = 4; // 購買張數，若無則選擇最大值
+const payType = "A"; // 付款方式：A=ATM, C=信用卡
+
+// 系統參數(勿動)
+let autoMode = localStorage.getItem("autoMode") || 0;
+let isSetAutoMode = false;
+let session = "";
+let isSelectArea = false;
+let isSelect2Button = false;
+let isClickBuyTicket = false;
+let isClickPayType = false;
+let isSubmit = false;
+
+// 取得當前網址
+const triggerUrl = window.location.href;
+if (triggerUrl.includes("activity/detail/")) {
+    session = "d";
+} else if (triggerUrl.includes("ticket/game/")) {
+    session = "g";
+} else if (triggerUrl.includes("ticket/verify/")) {
+    session = "v";
+} else if (triggerUrl.includes("ticket/area/")) {
+    session = "a";
+} else if (triggerUrl.includes("ticket/ticket/")) {
+    session = "t";
+} else if (triggerUrl.includes("ticket/checkout/")) {
+    session = "c";
+}
+
+// get query string
+const q = window.location.search;
+const urlParams = new URLSearchParams(q);
+if (urlParams.get("a") == 1) {
+    localStorage.setItem("autoMode", 1);
+    autoMode = 1;
+}
 
 (function () {
-    ("use strict");
-    // 個人參數
-    let url = "*"; // 指定啟用網址：*=永久啟用, ""=停用
-    let buyDate = "2024/11/22,2024/11/23"; // 購票日期：依順序以,隔開，*=不限 Ex. "2024/11/22,2024/11/23," // 先搶 11/22, 再搶 11/23, 都沒有則任一日期
-    let buyCount = 4;
-    let payType = "C"; // 付款方式：A=ATM, C=信用卡
-
-    // 系統參數
-    let checkCode = "40637634"; // 國泰世華信用卡驗證
-    let isClickBuyTicket = false;
-    let isClickPayType = false;
-    let isSubmit = false;
-
-    // 取得當前網址
-    const triggerUrl = window.location.href;
-    if (url == "*") {
-    } else if (triggerUrl.includes("tixcraft.com/activity/game/")) {
-        url = "game";
-    } else if (triggerUrl.includes("tixcraft.com/activity/perform/")) {
-        url = "perform";
-    } else if (triggerUrl.includes("tixcraft.com/activity/other/")) {
-        url = "other";
-    } else {
-        return;
-    }
-
-    function autoSubmit() {
-        const submit = document.querySelector("button[type=submit],#submitButton");
-        if (submit && !isSubmit) {
-            isSubmit = true;
-            submit.click();
-        }
-    }
-
+    "use strict";
     const observer = new MutationObserver((mutationsList) => {
         mutationsList.forEach((mutation) => {
             if (mutation.type === "childList") {
-                // observer.disconnect(); // 偵測到後停止監聽
+                observer.disconnect();
+                // 自動模式提示
+                autoModeTip();
 
-                const gameList = document.querySelector("#gameList table tbody");
-                if (gameList) {
-                    /*
-                    <table class="table table-bordered"><thead>
-<tr><th>演出時間</th><th>場次名稱</th><th>場地</th><th>購買狀態</th></tr>
-</thead>
-<tbody>
-<tr class="gridc fcTxt" data-key="17676"><td>2024/11/22 (五)  20:00</td><td>2024 YUGYEOM TOUR [TRUSTY] IN TAIPEI</td><td>Zepp New Taipei</td><td><button type="button" class="btn btn-primary text-bold m-0" data-href="https://tixcraft.com/ticket/area/24_yugyeom/17676">立即訂購</button></td></tr>
-</tbody></table>
-                    */
-                    // const gameRows = gameList.querySelectorAll("tr");
-
-                    // gameRows.forEach((row) => {
-                    //     const gameDate = row.querySelector("td").textContent;
-                    //     console.log(gameDate);
-                    //     if (gameDate.includes(buyDate)) {
-                    //         const gameButton = row.querySelector("button");
-                    //         if (gameButton && !isSubmit) {
-                    //             isSubmit = true;
-                    //             console.log("click");
-                    //             // gameButton.click();
-                    //         }
-                    //     }
-                    // });
-                }
-
-                // 勾選同意條款
-                const ticketAgree = document.getElementById("TicketForm_agree");
-                if (ticketAgree) {
-                    ticketAgree.checked = true;
-                }
-
-                // 輸入驗證碼：國泰信用卡卡號
-                const promoTitle = document.querySelector(".promo-title");
-                const promoDesc = document.querySelector(".promo-desc");
-                if (promoTitle && promoTitle.textContent.includes("國泰世華") && promoDesc && promoDesc.textContent.includes("卡號前8碼")) {
-                    const checkCodeInput = document.querySelector(".greyInput[name=checkCode]");
-                    if (checkCodeInput && checkCode.length > 0) {
-                        checkCodeInput.value = checkCode;
-                    }
-                }
-
-                // 輸入圖形驗證碼
-                const verifyCodeInput = document.getElementById("TicketForm_verifyCode");
-                if (verifyCodeInput) {
-                    verifyCodeInput.focus();
-                }
-
-                // 選取購票 4 張
-                const ticketPrice = document.querySelector('[id^="TicketForm_ticketPrice_"]');
-                if (ticketPrice) {
-                    ticketPrice.value = buyCount;
-                }
-
-                // 點選關閉提醒
+                // 自動關閉提醒
                 const closeAlert = document.querySelector("button.close-alert");
                 if (closeAlert) {
                     closeAlert.click();
                 }
 
-                // 點選立即購票
-                const buyTicket = document.querySelector('a[href^="/activity/game/"]');
-                if (buyTicket && !isClickBuyTicket) {
-                    isClickBuyTicket = true;
-                    buyTicket.click();
+                switch (session) {
+                    case "d":
+                    case "g":
+                        // 活動頁
+                        // 點選立即購票
+                        const buyTicket = document.querySelector('a[href^="/activity/game/"]');
+                        if (buyTicket && !isClickBuyTicket) {
+                            isClickBuyTicket = true;
+                            buyTicket.click();
+                        }
+
+                        const gameList = document.querySelector("#gameList table tbody");
+                        if (gameList && autoMode == 1 && !isSubmit) {
+                            goOrder(gameList);
+                        }
+                        break;
+                    case "v":
+                        // 輸入驗證碼：國泰信用卡卡號
+                        const promoTitle = document.querySelector(".promo-title");
+                        if (promoTitle && promoTitle.textContent.includes("國泰世華")) {
+                            const promoDesc = document.querySelector(".promo-desc");
+                            if (promoDesc && promoDesc.textContent.includes("卡號前8碼")) {
+                                const checkCodeInput = document.querySelector(".greyInput[name=checkCode]");
+                                if (checkCodeInput) {
+                                    checkCodeInput.value = "40637634"; // 國泰信用卡卡號前 8 碼
+                                    autoSubmit();
+                                }
+                            }
+                        }
+                        break;
+                    case "a":
+                        // 自動選位
+                        if (autoMode == 1 && !isSelectArea) {
+                            isSelectArea = true;
+                            const isOk = selectArea();
+                            if (!isOk) {
+                                setTimeout(() => {
+                                    window.location.reload();
+                                }, 100);
+                            }
+                        }
+                        // 選單按鈕化
+                        select2Button();
+                        // 隱藏已售完
+                        removeSoldOut();
+                        break;
+                    case "t":
+                        // 確認訂單
+                        // 勾選同意條款
+                        const ticketAgree = document.getElementById("TicketForm_agree");
+                        if (ticketAgree) {
+                            ticketAgree.checked = true;
+                        }
+
+                        // 選取購票張數
+                        const ticketPrice = document.querySelector('[id^="TicketForm_ticketPrice_"]');
+                        if (ticketPrice) {
+                            const options = ticketPrice.querySelectorAll("option");
+                            let hasBuyCount = false;
+                            options.forEach((o) => {
+                                if (o.value == buyCount) {
+                                    hasBuyCount = true;
+                                    ticketPrice.value = buyCount;
+                                }
+                            });
+                            if (!hasBuyCount) {
+                                ticketPrice.value = options[options.length - 1].value;
+                            }
+                        }
+
+                        // 輸入圖形驗證碼
+                        const verifyCodeInput = document.getElementById("TicketForm_verifyCode");
+                        if (verifyCodeInput) {
+                            verifyCodeInput.addEventListener("input", (e) => {
+                                if (e.target.value.length == 4) {
+                                    autoSubmit();
+                                }
+                            });
+
+                            verifyCodeInput.focus();
+                        }
+                        break;
+                    case "c":
+                        // 付款
+                        if (payType == "A") {
+                            // 選擇 ATM 付款
+                            const atmRadio = document.getElementById("CheckoutForm_paymentId_54");
+                            if (atmRadio && !isClickPayType) {
+                                isClickPayType = true;
+                                atmRadio.click();
+                            }
+                        } else if (payType == "C") {
+                            // 選擇信用卡付款
+                            const creditCardRadio = document.getElementById("CheckoutForm_paymentId_36");
+                            if (creditCardRadio && !isClickPayType) {
+                                isClickPayType = true;
+                                creditCardRadio.click();
+                            }
+                        }
+                        break;
                 }
 
-                if (payType == "A") {
-                    // 選擇 ATM 付款
-                    const atmRadio = document.getElementById("CheckoutForm_paymentId_54");
-                    if (atmRadio && !isClickPayType) {
-                        isClickPayType = true;
-                        atmRadio.click();
-                    }
-                } else if (payType == "C") {
-                    // 選擇信用卡付款
-                    const creditCardRadio = document.getElementById("CheckoutForm_paymentId_36");
-                    if (creditCardRadio && !isClickPayType) {
-                        isClickPayType = true;
-                        creditCardRadio.click();
-                    }
-                }
-
-                // submit button 變大
-                const submit = document.querySelector("button[type=submit],#submitButton");
-                if (submit) {
-                    submit.style.fontSize = "20px";
-                    submit.style.height = "100px";
-                    submit.style.width = "200px";
-                }
+                // 放大提交按鈕
+                largerSubmit();
             }
         });
+
+        function select2Button() {
+            const select = document.querySelector("#gameId");
+            if (select && !isSelect2Button) {
+                isSelect2Button = true;
+                // select.style.display = "none";
+                const title = document.querySelector(".activityT.title");
+                if (title) {
+                    title.style.display = "none";
+                }
+                const selectOptions = select.querySelectorAll("option");
+                selectOptions.forEach((option) => {
+                    const b = document.createElement("button");
+                    const dateText = option.textContent.match(/\d{4}\/\d{2}\/\d{2} \(\S+\)/);
+                    b.textContent = dateText ? dateText[0] : option.textContent;
+                    b.onclick = () => {
+                        select.value = option.value;
+                        select.dispatchEvent(new Event("change", { bubbles: true }));
+                    };
+                    b.style.padding = "2px 6px";
+                    b.style.margin = "2px";
+                    b.style.border = "1px solid #ccc";
+                    if (option.selected) {
+                        b.style.backgroundColor = "#007bff";
+                        b.style.color = "#fff";
+                    }
+
+                    select.before(b);
+                });
+            }
+        }
+
+        function selectArea() {
+            const areas = document.querySelectorAll(".area_select li a");
+            if (areas && areas.length > 0) {
+                for (let i = 0; i < buyArea.length; i++) {
+                    if (isSubmit) {
+                        break;
+                    }
+                    const buyAreaKeys = buyArea[i].split(" ");
+                    for (let j = 0; j < areas.length; j++) {
+                        const a = areas[j];
+                        const text = a.textContent;
+                        const remainFont = a.querySelector("font");
+                        if (remainFont) {
+                            const remainCount = remainFont.textContent.replace("剩餘 ", "");
+                            if (remainCount < buyCount) {
+                                continue;
+                            }
+                        }
+                        let matchCount = 0;
+                        buyAreaKeys.forEach((key) => {
+                            if (text.includes(key)) {
+                                matchCount++;
+                            }
+                        });
+                        if (!isSubmit && matchCount > 0 && matchCount == buyAreaKeys.length) {
+                            isSubmit = true;
+                            a.click();
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        function goOrder(gameList) {
+            let gameRows = gameList.querySelectorAll("tr");
+
+            for (let i = 0; i < buyDateIndexes.length; i++) {
+                let index = buyDateIndexes[i];
+                if (index >= gameRows.length) {
+                    // 預設最後一場
+                    index = gameRows.length - 1;
+                }
+
+                if (gameRows[index]) {
+                    const gameButton = gameRows[index].querySelector("button");
+                    if (gameButton && !isSubmit) {
+                        isSubmit = true;
+                        gameButton.click();
+                        break;
+                    }
+                }
+            }
+        }
+
+        function largerSubmit() {
+            const submit = document.querySelector("button[type=submit],#submitButton");
+            if (submit) {
+                submit.style.fontSize = "24px";
+                submit.style.height = "100px";
+                submit.style.width = "240px";
+            }
+        }
+
+        function removeSoldOut() {
+            const listItems = document.querySelectorAll("li");
+            const soldOutItems = Array.from(listItems).filter((li) => li.textContent.includes("已售完"));
+            soldOutItems.forEach((soldOut) => {
+                soldOut.remove();
+            });
+        }
+
+        function autoModeTip() {
+            if (isSetAutoMode) {
+                return;
+            }
+            isSetAutoMode = true;
+            const tip = document.createElement("div");
+            tip.style.position = "fixed";
+            tip.style.top = "0";
+            tip.style.left = "0";
+            tip.style.padding = "10px";
+            tip.style.textAlign = "center";
+            tip.style.zIndex = "9999";
+            tip.style.color = "white";
+            tip.style.cursor = "pointer";
+            if (autoMode == 1) {
+                tip.style.backgroundColor = "green";
+                tip.textContent = "🤖";
+            } else {
+                tip.style.backgroundColor = "red";
+                tip.textContent = "Manual";
+            }
+            document.body.appendChild(tip);
+            tip.addEventListener("click", () => {
+                localStorage.setItem("autoMode", autoMode == 1 ? 0 : 1);
+                window.location.reload();
+            });
+        }
     });
 
     observer.observe(document.body, { childList: true, subtree: true });
@@ -165,3 +325,11 @@
         }
     });
 })();
+
+function autoSubmit() {
+    const submit = document.querySelector("button[type=submit],#submitButton");
+    if (submit && !isSubmit) {
+        isSubmit = true;
+        submit.click();
+    }
+}
