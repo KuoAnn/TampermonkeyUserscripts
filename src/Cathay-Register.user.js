520f42ebf517a0d0a288bb66280b4938a8760dbb
KuoAnn <b54744040@gmail.com>
2024-10-06 17:22:08 +0800

    國泰卡友快速登錄

--- src/Cathay-Register.user.js
+++ src/Cathay-Register.user.js
@@ -2,46 +2,172 @@
 // @name         Cathay Auto Register
 // @namespace    https://www.cathaybk.com.tw/promotion/CreditCard/Event
 // @source       https://github.com/KuoAnn/TampermonkeyUserscripts/raw/main/src/Cathay-Register.user.js
-// @version      1.0.1
+// @version      1.0.2
 // @description  try to take over the world!
 // @author       KuoAnn
-// @match        https://www.cathaybk.com.tw/promotion*/CreditCard/Event
-// @grant        none
+// @match        https://www.cathaybk.com.tw/promotion*
+// @grant        GM_xmlhttpRequest
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.cathaybk.com.tw
 // ==/UserScript==
 
+// 個人參數
+const USER_ID = "";
+const USER_BIRTH = "";
+
+// 系統參數
+const CAPTCHA_API_URL = "http://maxbot.dropboxlike.com:16888/ocr";
+const CAPTCHA_INPUT_SELECTOR = "#Captcha";
+const CAPTCHA_IMAGE_SELECTOR = "#captchaIcon";
+const CAPTCHA_REFRESH_SELECTOR = "#captcha-refresh";
+const SUBMIT_SELECTOR = "input[type='submit']";
+let _captchaBase64 = "";
+let _isLoaded = false;
+
 (function () {
     "use strict";
-    var autoReload = false;
-    var reloadWaitTime = 3;
 
-    var start = function () {
-        var attack = function (cid) {
-            if (!!cid) {
-                alert("Attack " + cid);
-                var form = $("#form-hidden");
-                var token = form.find('input[name="__RequestVerificationToken"]').val();
+    const observer = new MutationObserver((mo) => {
+        mo.forEach((mutation) => {
+            if (mutation.type === "childList") {
+                console.log("Mutated");
+
+                const captchaImage = document.querySelector(CAPTCHA_IMAGE_SELECTOR);
+                if (!_isLoaded && captchaImage) {
+                    _isLoaded = true;
+                    if (captchaImage) {
+                        _captchaBase64 = getCaptchaImage(captchaImage);
+                        if (_captchaBase64) {
+                            setCaptchaAndSubmit(_captchaBase64, "#Captcha");
+                        }
+                    }
+
+                    const idInput = document.querySelector(".input-element#ID");
+                    if (idInput) {
+                        idInput.value = USER_ID;
+                    }
+
+                    const birthInput = document.querySelector(".input-element#BirthDate");
+                    if (birthInput) {
+                        birthInput.value = USER_BIRTH;
+                    }
+
+                    const checkAgree = document.querySelector(".checkbox#CheckAgreement");
+                    if (checkAgree) {
+                        checkAgree.checked = true;
+                    }
+                }
+
+                const registerBtns = document.querySelectorAll(".btn.btn-sign");
+                if (!_isLoaded && registerBtns.length > 0) {
+                    _isLoaded = true;
+                    console.log("Register Loaded");
+                    registerBtns.forEach((btn) => {
+                        var cid = btn.getAttribute("data-campaign-id");
+                        if (cid) {
+                            try {
+                                // attack(cid);
+                                var title = btn.closest(".tr").querySelector(".td.wtitle a").textContent.replace("\r\n", "").trim();
+                                alert("註冊 " + title);
+                            } catch (error) {
+                                console.error(error);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        function getCaptchaImage() {
+            const element = document.querySelector(CAPTCHA_IMAGE_SELECTOR);
+            if (element) {
+                const canvas = document.createElement("canvas");
+                const context = canvas.getContext("2d");
+                canvas.height = element.naturalHeight;
+                canvas.width = element.naturalWidth;
+                context.drawImage(element, 0, 0);
+                const img_data = canvas.toDataURL();
+                return img_data ? img_data.split(",")[1] : "";
+            }
+            return "";
+        }
+
+        function setCaptchaAndSubmit(image_data) {
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: CAPTCHA_API_URL,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                data: JSON.stringify({ image_data: image_data }),
+                onload: function (r) {
+                    console.log(r.responseText);
+                    if (r.status == 200) {
+                        let answer = JSON.parse(r.responseText).answer;
+                        // auto guess
+                        answer = answer.replace(/[gq]/g, "9");
+                        if (answer && answer.match(/^\d{4}$/)) {
+                            const captchaInput = document.querySelector(CAPTCHA_INPUT_SELECTOR);
+                            if (captchaInput) {
+                                captchaInput.value = answer;
+                                const submitButton = document.querySelector(SUBMIT_SELECTOR);
+                                if (submitButton) {
+                                    submitButton.click();
+                                }
+                            }
+                        } else {
+                            refreshCaptcha();
+                        }
+                    } else {
+                        console.error(" Fail", r.statusText + "|" + r.responseText);
+                    }
+                },
+                onerror: function (error) {
+                    console.error(" Error:", error);
+                },
+            });
+        }
+
+        function refreshCaptcha() {
+            const imgCaptcha = document.querySelector(CAPTCHA_REFRESH_SELECTOR);
+            if (imgCaptcha) {
+                console.log("Refresh Captcha");
+                imgCaptcha.click();
+
+                const interval = setInterval(() => {
+                    const image_data = getCaptchaImage();
+                    if (image_data !== "" && _captchaBase64 && image_data !== _captchaBase64) {
+                        clearInterval(interval);
+                        setCaptchaAndSubmit(image_data);
+                    }
+                }, 1000);
+            }
+        }
+
+        const attack = function (campaignId) {
+            if (campaignId) {
+                alert(`Attack ${campaignId}`);
+                const form = $("#form-hidden");
+                const token = form.find('input[name="__RequestVerificationToken"]').val();
 
                 $.ajax({
                     url: form[0].action,
                     type: form[0].method,
-                    cache: 0,
-                    data: "CampaignId=" + cid + "&__RequestVerificationToken=" + token,
+                    cache: false,
+                    data: `CampaignId=${campaignId}&__RequestVerificationToken=${token}`,
                     dataType: "json",
-                    success: function (rs) {
-                        if (rs) {
-                            alert(rs.signResultText);
+                    success: function (response) {
+                        if (response) {
+                            alert(response.signResultText);
 
-                            if (rs.signResult > 1) {
+                            if (response.signResult > 1) {
                                 // re-attack
-                                attack(cid);
-                                attack(cid);
+                                attack(campaignId);
                             }
 
-                            switch (rs.signResult) {
+                            switch (response.signResult) {
                                 case 0:
                                 case 1:
-                                    alert("<h1 style='color:green'>SUCCESS! SN=" + rs.signUpNumber + "</h1>");
+                                    alert(`<h1 style='color:green'>SUCCESS! SN=${response.signUpNumber}</h1>`);
                                     break;
                                 case 9001:
                                     alert("Full");
@@ -53,6 +179,7 @@
                                     alert("Busy");
                                     break;
                                 default:
+                                    alert("Unknown result");
                             }
                         }
                     },
@@ -68,68 +195,48 @@
                 });
             }
         };
+    });
 
-        $(".btn.btn-sign").each(function (i, d) {
-            var cid = $(d).attr("data-campaign-id");
+    alert = (function () {
+        var alertContainer = document.createElement("div");
+        alertContainer.style.position = "fixed";
+        alertContainer.style.top = "0";
+        alertContainer.style.left = "0";
+        alertContainer.style.zIndex = "9999";
+        alertContainer.style.pointerEvents = "none";
+        document.body.appendChild(alertContainer);
 
-            if (!!cid) {
-                attack(cid);
-                attack(cid);
-                alert("Found " + cid);
-            }
-        });
-    };
+        var messages = [];
 
-    alert = function (str, alertType) {
-        function checkTime(i) {
-            return i < 10 ? "0" + i : i;
-        }
-        function getNowTime() {
-            var today = new Date(),
-                h = checkTime(today.getHours()),
-                m = checkTime(today.getMinutes()),
-                s = checkTime(today.getSeconds());
-            return h + ":" + m + ":" + s;
-        }
+        return function (str) {
+            var message = document.createElement("div");
+            message.style.background = "rgba(0, 0, 0, 0.7)";
+            message.style.color = "white";
+            message.style.padding = "4px";
+            message.style.margin = "2px";
+            message.style.borderRadius = "5px";
+            message.style.pointerEvents = "auto";
+            message.style.fontSize = "14px";
+            message.innerText = str;
 
-        var oTest = document.getElementsByClassName("tabbox")[0];
-        var newNode = document.createElement("div");
-        newNode.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
-        if (alertType == "red") {
-            newNode.style.color = "red";
-            newNode.style.fontWeight = "bold";
-            newNode.style.backgroundColor = "yellow";
-        }
+            alertContainer.appendChild(message);
+            let currentTime = new Date().toLocaleTimeString("en-GB", { hour12: false });
+            messages.push(`${currentTime} ${message}`);
 
-        newNode.innerHTML = getNowTime() + " >>> " + str;
-
-        oTest.insertBefore(newNode, oTest.childNodes[0]);
-    };
-
-    var waitHtmlBuzzer = setInterval(function () {
-        alert("Loading...");
-        if (document.getElementsByClassName("btn").length > 0) {
-            clearInterval(waitHtmlBuzzer);
-
-            if (document.getElementsByClassName("btn-sign").length == 0) {
-                alert("查無可登錄的活動", "red");
-
-                if (autoReload) {
-                    var reloadTimer = setInterval(function () {
-                        if (reloadWaitTime < 1) {
-                            clearInterval(reloadTimer);
-                            alert("Reloading...");
-                            location.reload();
-                            return;
-                        } else {
-                            alert("Wait to Reload..." + reloadWaitTime--);
-                        }
-                    }, 1000);
-                    return;
+            if (messages.length > 20) {
+                var oldMessage = messages.shift();
+                if (alertContainer.contains(oldMessage)) {
+                    alertContainer.removeChild(oldMessage);
                 }
-            } else {
-                start();
             }
-        }
-    }, 100);
+
+            setTimeout(function () {
+                if (alertContainer.contains(message)) {
+                    alertContainer.removeChild(message);
+                }
+            }, 5000);
+        };
+    })();
+
+    observer.observe(document.body, { childList: true, subtree: true });
 })();
